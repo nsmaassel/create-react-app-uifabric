4acd51da0a7447339e26288cb880d9660660527c
Nick Maassel <Nicholas.Maassel@TheSpurGroup.com>
2020-12-23 13:58:22 -0800

    Adds proper coachmark functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-    Breadcrumb, Coachmark,
+    Breadcrumb,
+    Coachmark,
     DefaultButton,
+    DirectionalHint,
     Dropdown,
     IBreadcrumbItem,
+    IButtonProps,
     Icon,
     IDividerAsProps,
     IDropdownOption,
-    IDropdownStyles,
+    IDropdownStyles, IIconProps,
     initializeIcons,
-    PrimaryButton,
+    PrimaryButton, TeachingBubble,
+    TeachingBubbleContent,
     TooltipHost
 } from 'office-ui-fabric-react';
 import msftLogo from './assets/microsoftLogoLight.png';
 import styles from './App.module.scss';
 import businessCentral from './data/businessCentralSteps.json';
+import {useBoolean} from '@uifabric/react-hooks';
 
 // Fixes missing chevron in dropdown menu
 initializeIcons();
@@ -25,13 +30,16 @@ const dropdownStyles: Partial<IDropdownStyles> = {
 };
 
 const options: IDropdownOption[] = [
-    {key: '1', text: 'Welcome'},
-    {key: '2', text: 'Understand the health of your business'},
-    {key: '3', text: 'Simplified process with Excel integration'},
-    {key: '4', text: 'Improved communication with Outlook integration'},
-    {key: '5', text: 'Intuitive features to drive efficiency'},
-    {key: '6', text: 'Summary'},
+    {key: 1, text: 'Welcome'},
+    {key: 2, text: 'Understand the health of your business'},
+    {key: 3, text: 'Simplified process with Excel integration'},
+    {key: 4, text: 'Improved communication with Outlook integration'},
+    {key: 5, text: 'Intuitive features to drive efficiency'},
+    {key: 6, text: 'Summary'},
 ];
+
+const screenExpandIcon: IIconProps = {iconName: 'FullScreen'};
+const screenCollapseIcon: IIconProps = {iconName: 'BackToWindow'};
 
 interface IProgramStep {
     stepNumber: number,
@@ -86,7 +94,7 @@ export const App: React.FunctionComponent = () => {
         },
     ];
 
-    function incrementStep() {
+    const incrementStep = () => {
         let currentStepNumber = state.currentStep.stepNumber;
 
         let nextStep = state.currentTour.steps[currentStepNumber];
@@ -95,9 +103,11 @@ export const App: React.FunctionComponent = () => {
             ...state,
             currentStep: nextStep
         });
+
+        onStepChange();
     }
 
-    function decrementStep() {
+    const decrementStep = () => {
         let currentStepNumber = state.currentStep.stepNumber;
 
         let nextStep = state.currentTour.steps[currentStepNumber - 2];
@@ -106,9 +116,49 @@ export const App: React.FunctionComponent = () => {
             ...state,
             currentStep: nextStep
         });
+
+        onStepChange();
     }
 
-    const markerTarget = React.useRef<HTMLDivElement>(null);
+    const targetButton = React.useRef<HTMLDivElement>(null);
+    const [isCoachmarkVisible, {setFalse: hideCoachmark, setTrue: showCoachmark}] = useBoolean(true);
+    const [coachmarkPosition, setCoachmarkPosition] = React.useState<DirectionalHint>(DirectionalHint.bottomAutoEdge);
+    const [isTeachingBubbleVisible, {setFalse: hideTeachingBubble, setTrue: showTeachingBubble}] = useBoolean(false);
+    // const [isTeachingBubbleVisible, setIsTeachingBubbleVisible] = useState(false);
+
+    const onStepChange = React.useCallback(
+        (): void => {
+            setCoachmarkPosition(DirectionalHint.bottomAutoEdge);
+        },
+        [],
+    );
+
+    useEffect(() => {
+        console.log('Current step update: useEffect');
+
+    }, [state.currentStep])
+
+    const positioningContainerProps = React.useMemo(
+        () => ({
+            directionalHint: coachmarkPosition,
+            doNotLayer: false,
+        }),
+        [state.currentStep],
+    );
+
+    const prevButtonProps: IButtonProps = {
+        text: '< Prev',
+        onClick: () => {
+            decrementStep()
+        },
+    };
+
+    const nextButtonProps: IButtonProps = {
+        text: 'Next >',
+        onClick: () => {
+            incrementStep()
+        },
+    };
 
     return (
         <div className={styles.tourContainer}>
@@ -117,22 +167,31 @@ export const App: React.FunctionComponent = () => {
                     <img src={msftLogo} className={styles.logoIcon} alt={'Microsoft'}/>
                     <div className={styles.title}>{state.currentTour.title} Guided Tour</div>
                 </div>
+
                 <div className={styles.buttons}>
                     <DefaultButton
                         className={styles.requestTrialButton}
                         text="Request a trial"
                         onClick={() => {
                         }}
-                        allowDisabledFocus/>
+                        allowDisabledFocus
+                    />
                     <DefaultButton
                         className={styles.displayModeButton}
-                        text="Full Screen Mode"
+                        text={state.fullscreenMode ? "Standard Mode" : "Full Screen Mode"}
                         onClick={() => {
+                            setState({
+                                ...state,
+                                fullscreenMode: !state.fullscreenMode
+                            })
                         }}
-                        allowDisabledFocus/>
+                        allowDisabledFocus
+                        iconProps={state.fullscreenMode ? screenCollapseIcon : screenExpandIcon}
+                    />
                 </div>
             </div>
             <div className={styles.body}>
+                {!state.fullscreenMode &&
                 <div className={styles.leftPane}>
                     <Breadcrumb
                         className={styles.breadcrumbs}
@@ -145,17 +204,20 @@ export const App: React.FunctionComponent = () => {
                         options={options}
                         styles={dropdownStyles}
                         selectedKey={state.currentStep.stepNumber}
+                        onChanged={(item, key) => setState({
+                            ...state,
+                            currentStep: state.currentTour.steps[key ? key : 1],
+                        })}
                     />
                     <p className={styles.stepTitle}>{state.currentStep.stepName}</p>
                     <p>Step {state.currentStep.stepNumber} of {state.currentTour.stepCount}</p>
-                    <p>Dynamics 365 Business Central is designed to empower you to make quicker,
-                        better-informed decisions. With deep integration with tools like Outlook and Excel,
-                        an intuitive interface, and AI-driven insights, Business Central makes it easy to manage sales,
-                        inventory, and other processes as you chart your business growth.
+                    <p>{state.currentStep.stepDetails}
                     </p>
+                    {state.currentStep.stepInstructions &&
                     <p className={styles.stepInstructions}>
-                        Select the dot or the Next button to progress to the next step.
+                        {state.currentStep.stepInstructions}
                     </p>
+                    }
                     {state.currentStep.stepNumber > 1 ?
                         <DefaultButton
                             className={styles.prevButton}
@@ -175,34 +237,91 @@ export const App: React.FunctionComponent = () => {
                         /> : <></>
                     }
                 </div>
+                }
                 <div className={styles.rightPane}>
                     <img
                         src={require(`./assets/${state.currentTour.title.toLowerCase().replace(/\s+/g, '')}/${state.currentStep.stepNumber}.png`)}
                         alt={state.currentStep.stepName}
                         className={styles.stepImage}
                     />
-                    {/*<Coachmark target={markerLight}/>*/}
-                </div>
-            </div>
-        </div>
-    );
-};
 
-function _onBreadcrumbItemClicked(ev: React.MouseEvent<HTMLElement>, item: IBreadcrumbItem): void {
-    console.log(`Breadcrumb item with key "${item.key}" has been clicked.`);
-}
+                    {/*Hidden by transparency, just used for coachmark positioning/target*/}
+                    <div
+                        className={styles.buttonContainer}
+                        ref={targetButton}
+                        style={{
+                            left: state.currentStep.coachMarkCoordinates[0] + '%',
+                            top: state.currentStep.coachMarkCoordinates[1] + '%',
+                        }}
+                    >
+                        <DefaultButton
+                            id='markerButton'
+                            className={styles.markerButton}
+                            onClick={showCoachmark}
+                            text={isCoachmarkVisible ? 'Hide coachmark' : 'Show coachmark'}
+                        >{state.currentStep.stepNumber}</DefaultButton>
+                    </div>
 
-function _getCustomOverflowIcon(): JSX.Element {
-    return <Icon iconName={'ChevronDown'}/>;
-}
+                    {state.currentTour.steps.map((step) => {
+                        return (
+                            state.currentStep.stepNumber === step.stepNumber ? (
+                            <Coachmark
+                                target={'#markerButton'}
+                                positioningContainerProps={positioningContainerProps}
+                                preventDismissOnLostFocus={true}
+                                preventFocusOnMount={true}
+                            >
+                                {
+                                    // state.fullscreenMode &&
+                                    // isTeachingBubbleVisible &&
+                                    <TeachingBubbleContent
+                                        headline={step.stepName}
+                                        secondaryButtonProps={step.stepNumber > 1 ? prevButtonProps : undefined}
+                                        primaryButtonProps={step.stepNumber < state.currentTour.stepCount ? nextButtonProps : undefined}
+                                        hasCloseIcon={!state.fullscreenMode}
+                                        onDismiss={() => {
+                                            hideCoachmark();
+                                            hideTeachingBubble();
+                                        }
+                                        }
+                                    >
+                                        <p>Step {step.stepNumber} of {state.currentTour.stepCount}</p>
+                                        <p>{step.stepDetails}
+                                        </p>
+                                        {step.stepInstructions &&
+                                        <p className={styles.stepInstructions}>
+                                            {step.stepInstructions}
+                                        </p>
+                                        }
+                                    </TeachingBubbleContent>
+                                }
+                            </Coachmark>
+                            ) : <></>
+                        )
+                    })}
+                        </div>
+                        </div>
+                        </div>
+                        )
+                        ;
+                    }
+                    ;
 
-function _getCustomDivider(dividerProps: IDividerAsProps): JSX.Element {
-    const tooltipText = dividerProps.item ? dividerProps.item.text : '';
-    return (
-        <TooltipHost content={`Show ${tooltipText} contents`} calloutProps={{gapSpace: 0}}>
-      <span aria-hidden="true" style={{cursor: 'pointer', padding: 5}}>
-        /
-      </span>
-        </TooltipHost>
-    );
-}
+                    function _onBreadcrumbItemClicked(ev: React.MouseEvent<HTMLElement>, item: IBreadcrumbItem): void {
+                    console.log(`Breadcrumb item with key "${item.key}" has been clicked.`);
+                }
+
+                    function _getCustomOverflowIcon(): JSX.Element {
+                    return <Icon iconName={'ChevronDown'}/>;
+                }
+
+                    function _getCustomDivider(dividerProps: IDividerAsProps): JSX.Element {
+                    const tooltipText = dividerProps.item ? dividerProps.item.text : '';
+                    return (
+                    <TooltipHost content={`Show ${tooltipText} contents`} calloutProps={{gapSpace: 0}}>
+                    <span aria-hidden="true" style={{cursor: 'pointer', padding: 5}}>
+                    /
+                    </span>
+                    </TooltipHost>
+                    );
+                }
